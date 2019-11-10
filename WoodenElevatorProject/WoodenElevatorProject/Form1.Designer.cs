9913cbfb4511c6c419b5729f9f4a0921ad39c9d1
Crstha <48519397+Crstha@users.noreply.github.com>
2019-11-10 12:52:59 +0545

    Add files via upload

---
+++ WoodenElevatorProject/WoodenElevatorProject/Form1.Designer.cs
@@ -0,0 +1,369 @@
+﻿namespace WoodenElevatorProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnlControlPanel = new System.Windows.Forms.Panel();
+            this.btnAlarm = new System.Windows.Forms.Button();
+            this.pnlDisplayPanel = new System.Windows.Forms.Panel();
+            this.TimerFirstFloorDoorOpen = new System.Windows.Forms.Timer(this.components);
+            this.TimerFirstFloorDoorClose = new System.Windows.Forms.Timer(this.components);
+            this.TimerGFloorDoorOpen = new System.Windows.Forms.Timer(this.components);
+            this.TimerGFloorDoorClose = new System.Windows.Forms.Timer(this.components);
+            this.TimerLiftDown = new System.Windows.Forms.Timer(this.components);
+            this.TimerLiftUp = new System.Windows.Forms.Timer(this.components);
+            this.pnlOuterDesign = new System.Windows.Forms.Panel();
+            this.pnlUpr = new System.Windows.Forms.Panel();
+            this.pnlLwr = new System.Windows.Forms.Panel();
+            this.pnlBtm = new System.Windows.Forms.Panel();
+            this.pnlMiddle = new System.Windows.Forms.Panel();
+            this.pnlShaft = new System.Windows.Forms.Panel();
+            this.picLwrRightDoor = new System.Windows.Forms.PictureBox();
+            this.picLwrLeftDoor = new System.Windows.Forms.PictureBox();
+            this.picUprRightDoor = new System.Windows.Forms.PictureBox();
+            this.picUprLeftDoor = new System.Windows.Forms.PictureBox();
+            this.pnlLift = new System.Windows.Forms.Panel();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnG = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.pnlControlPanel.SuspendLayout();
+            this.pnlOuterDesign.SuspendLayout();
+            this.pnlShaft.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picLwrRightDoor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLwrLeftDoor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picUprRightDoor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picUprLeftDoor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pnlOuterDesign);
+            this.panel1.Controls.Add(this.btnDown);
+            this.panel1.Controls.Add(this.btnUp);
+            this.panel1.Controls.Add(this.pnlControlPanel);
+            this.panel1.Location = new System.Drawing.Point(1, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1077, 530);
+            this.panel1.TabIndex = 0;
+            // 
+            // pnlControlPanel
+            // 
+            this.pnlControlPanel.Controls.Add(this.btnAlarm);
+            this.pnlControlPanel.Controls.Add(this.btnG);
+            this.pnlControlPanel.Controls.Add(this.btn1);
+            this.pnlControlPanel.Controls.Add(this.btnClose);
+            this.pnlControlPanel.Controls.Add(this.btnOpen);
+            this.pnlControlPanel.Controls.Add(this.pnlDisplayPanel);
+            this.pnlControlPanel.Location = new System.Drawing.Point(311, 61);
+            this.pnlControlPanel.Name = "pnlControlPanel";
+            this.pnlControlPanel.Size = new System.Drawing.Size(183, 385);
+            this.pnlControlPanel.TabIndex = 1;
+            // 
+            // btnAlarm
+            // 
+            this.btnAlarm.Location = new System.Drawing.Point(62, 295);
+            this.btnAlarm.Name = "btnAlarm";
+            this.btnAlarm.Size = new System.Drawing.Size(43, 38);
+            this.btnAlarm.TabIndex = 5;
+            this.btnAlarm.UseVisualStyleBackColor = true;
+            this.btnAlarm.Click += new System.EventHandler(this.btnAlarm_Click);
+            // 
+            // pnlDisplayPanel
+            // 
+            this.pnlDisplayPanel.Location = new System.Drawing.Point(15, 14);
+            this.pnlDisplayPanel.Name = "pnlDisplayPanel";
+            this.pnlDisplayPanel.Size = new System.Drawing.Size(154, 90);
+            this.pnlDisplayPanel.TabIndex = 0;
+            // 
+            // TimerFirstFloorDoorOpen
+            // 
+            this.TimerFirstFloorDoorOpen.Interval = 10;
+            this.TimerFirstFloorDoorOpen.Tick += new System.EventHandler(this.TimerFirstFloorDoorOpen_Tick);
+            // 
+            // TimerFirstFloorDoorClose
+            // 
+            this.TimerFirstFloorDoorClose.Interval = 10;
+            this.TimerFirstFloorDoorClose.Tick += new System.EventHandler(this.TimerFirstFloorDoorClose_Tick);
+            // 
+            // TimerGFloorDoorOpen
+            // 
+            this.TimerGFloorDoorOpen.Interval = 10;
+            this.TimerGFloorDoorOpen.Tick += new System.EventHandler(this.TimerGFloorDoorOpen_Tick);
+            // 
+            // TimerGFloorDoorClose
+            // 
+            this.TimerGFloorDoorClose.Interval = 10;
+            this.TimerGFloorDoorClose.Tick += new System.EventHandler(this.TimerGFloorDoorClose_Tick);
+            // 
+            // TimerLiftDown
+            // 
+            this.TimerLiftDown.Interval = 10;
+            this.TimerLiftDown.Tick += new System.EventHandler(this.TimerLiftDown_Tick);
+            // 
+            // TimerLiftUp
+            // 
+            this.TimerLiftUp.Interval = 10;
+            this.TimerLiftUp.Tick += new System.EventHandler(this.TimerLiftUp_Tick);
+            // 
+            // pnlOuterDesign
+            // 
+            this.pnlOuterDesign.BackgroundImage = global::WoodenElevatorProject.Properties.Resources.ddd;
+            this.pnlOuterDesign.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlOuterDesign.Controls.Add(this.pnlUpr);
+            this.pnlOuterDesign.Controls.Add(this.pnlLwr);
+            this.pnlOuterDesign.Controls.Add(this.pnlBtm);
+            this.pnlOuterDesign.Controls.Add(this.pnlMiddle);
+            this.pnlOuterDesign.Controls.Add(this.pnlShaft);
+            this.pnlOuterDesign.Location = new System.Drawing.Point(45, 11);
+            this.pnlOuterDesign.Name = "pnlOuterDesign";
+            this.pnlOuterDesign.Size = new System.Drawing.Size(202, 509);
+            this.pnlOuterDesign.TabIndex = 0;
+            // 
+            // pnlUpr
+            // 
+            this.pnlUpr.BackgroundImage = global::WoodenElevatorProject.Properties.Resources.upper;
+            this.pnlUpr.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlUpr.Location = new System.Drawing.Point(23, 267);
+            this.pnlUpr.Name = "pnlUpr";
+            this.pnlUpr.Size = new System.Drawing.Size(162, 24);
+            this.pnlUpr.TabIndex = 1;
+            // 
+            // pnlLwr
+            // 
+            this.pnlLwr.BackgroundImage = global::WoodenElevatorProject.Properties.Resources.upper;
+            this.pnlLwr.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlLwr.Location = new System.Drawing.Point(23, 229);
+            this.pnlLwr.Name = "pnlLwr";
+            this.pnlLwr.Size = new System.Drawing.Size(162, 12);
+            this.pnlLwr.TabIndex = 0;
+            // 
+            // pnlBtm
+            // 
+            this.pnlBtm.BackgroundImage = global::WoodenElevatorProject.Properties.Resources.upper;
+            this.pnlBtm.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlBtm.Location = new System.Drawing.Point(23, 492);
+            this.pnlBtm.Name = "pnlBtm";
+            this.pnlBtm.Size = new System.Drawing.Size(162, 14);
+            this.pnlBtm.TabIndex = 7;
+            // 
+            // pnlMiddle
+            // 
+            this.pnlMiddle.BackgroundImage = global::WoodenElevatorProject.Properties.Resources.WoodenTexture1;
+            this.pnlMiddle.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlMiddle.Location = new System.Drawing.Point(0, 240);
+            this.pnlMiddle.Name = "pnlMiddle";
+            this.pnlMiddle.Size = new System.Drawing.Size(202, 28);
+            this.pnlMiddle.TabIndex = 1;
+            // 
+            // pnlShaft
+            // 
+            this.pnlShaft.BackgroundImage = global::WoodenElevatorProject.Properties.Resources.WoodenTexture;
+            this.pnlShaft.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlShaft.Controls.Add(this.picLwrRightDoor);
+            this.pnlShaft.Controls.Add(this.picLwrLeftDoor);
+            this.pnlShaft.Controls.Add(this.picUprRightDoor);
+            this.pnlShaft.Controls.Add(this.picUprLeftDoor);
+            this.pnlShaft.Controls.Add(this.pnlLift);
+            this.pnlShaft.Location = new System.Drawing.Point(24, 21);
+            this.pnlShaft.Name = "pnlShaft";
+            this.pnlShaft.Size = new System.Drawing.Size(161, 475);
+            this.pnlShaft.TabIndex = 0;
+            // 
+            // picLwrRightDoor
+            // 
+            this.picLwrRightDoor.BackgroundImage = global::WoodenElevatorProject.Properties.Resources.RDoor;
+            this.picLwrRightDoor.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picLwrRightDoor.Location = new System.Drawing.Point(83, 268);
+            this.picLwrRightDoor.Name = "picLwrRightDoor";
+            this.picLwrRightDoor.Size = new System.Drawing.Size(92, 207);
+            this.picLwrRightDoor.TabIndex = 3;
+            this.picLwrRightDoor.TabStop = false;
+            // 
+            // picLwrLeftDoor
+            // 
+            this.picLwrLeftDoor.BackgroundImage = global::WoodenElevatorProject.Properties.Resources.Ldoor;
+            this.picLwrLeftDoor.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picLwrLeftDoor.Location = new System.Drawing.Point(-24, 265);
+            this.picLwrLeftDoor.Name = "picLwrLeftDoor";
+            this.picLwrLeftDoor.Size = new System.Drawing.Size(107, 207);
+            this.picLwrLeftDoor.TabIndex = 2;
+            this.picLwrLeftDoor.TabStop = false;
+            // 
+            // picUprRightDoor
+            // 
+            this.picUprRightDoor.BackgroundImage = global::WoodenElevatorProject.Properties.Resources.RDoor;
+            this.picUprRightDoor.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picUprRightDoor.Location = new System.Drawing.Point(83, 0);
+            this.picUprRightDoor.Name = "picUprRightDoor";
+            this.picUprRightDoor.Size = new System.Drawing.Size(92, 213);
+            this.picUprRightDoor.TabIndex = 1;
+            this.picUprRightDoor.TabStop = false;
+            // 
+            // picUprLeftDoor
+            // 
+            this.picUprLeftDoor.BackgroundImage = global::WoodenElevatorProject.Properties.Resources.Ldoor;
+            this.picUprLeftDoor.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picUprLeftDoor.Location = new System.Drawing.Point(-24, 0);
+            this.picUprLeftDoor.Name = "picUprLeftDoor";
+            this.picUprLeftDoor.Size = new System.Drawing.Size(107, 213);
+            this.picUprLeftDoor.TabIndex = 0;
+            this.picUprLeftDoor.TabStop = false;
+            // 
+            // pnlLift
+            // 
+            this.pnlLift.BackgroundImage = global::WoodenElevatorProject.Properties.Resources.Lift;
+            this.pnlLift.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlLift.Location = new System.Drawing.Point(0, -2);
+            this.pnlLift.Name = "pnlLift";
+            this.pnlLift.Size = new System.Drawing.Size(161, 221);
+            this.pnlLift.TabIndex = 2;
+            // 
+            // btnDown
+            // 
+            this.btnDown.BackgroundImage = global::WoodenElevatorProject.Properties.Resources.liftOpen;
+            this.btnDown.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnDown.Location = new System.Drawing.Point(251, 356);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(43, 38);
+            this.btnDown.TabIndex = 6;
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // btnUp
+            // 
+            this.btnUp.BackgroundImage = global::WoodenElevatorProject.Properties.Resources.liftOpen;
+            this.btnUp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnUp.Location = new System.Drawing.Point(250, 116);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(43, 38);
+            this.btnUp.TabIndex = 2;
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // btnG
+            // 
+            this.btnG.BackgroundImage = global::WoodenElevatorProject.Properties.Resources.btnG;
+            this.btnG.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnG.Location = new System.Drawing.Point(94, 232);
+            this.btnG.Name = "btnG";
+            this.btnG.Size = new System.Drawing.Size(43, 38);
+            this.btnG.TabIndex = 4;
+            this.btnG.UseVisualStyleBackColor = true;
+            this.btnG.Click += new System.EventHandler(this.btnG_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.BackgroundImage = global::WoodenElevatorProject.Properties.Resources.btn1;
+            this.btn1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn1.Location = new System.Drawing.Point(28, 232);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(43, 38);
+            this.btn1.TabIndex = 3;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackgroundImage = global::WoodenElevatorProject.Properties.Resources.liftClose;
+            this.btnClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnClose.Location = new System.Drawing.Point(94, 162);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(43, 38);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.BackgroundImage = global::WoodenElevatorProject.Properties.Resources.liftOpen;
+            this.btnOpen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnOpen.Location = new System.Drawing.Point(28, 161);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(43, 38);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1078, 533);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.pnlControlPanel.ResumeLayout(false);
+            this.pnlOuterDesign.ResumeLayout(false);
+            this.pnlShaft.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picLwrRightDoor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLwrLeftDoor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picUprRightDoor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picUprLeftDoor)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnlOuterDesign;
+        private System.Windows.Forms.Panel pnlShaft;
+        private System.Windows.Forms.Panel pnlLwr;
+        private System.Windows.Forms.Panel pnlUpr;
+        private System.Windows.Forms.Panel pnlLift;
+        private System.Windows.Forms.PictureBox picUprRightDoor;
+        private System.Windows.Forms.PictureBox picUprLeftDoor;
+        private System.Windows.Forms.PictureBox picLwrRightDoor;
+        private System.Windows.Forms.PictureBox picLwrLeftDoor;
+        private System.Windows.Forms.Panel pnlControlPanel;
+        private System.Windows.Forms.Button btnAlarm;
+        private System.Windows.Forms.Button btnG;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Panel pnlDisplayPanel;
+        private System.Windows.Forms.Panel pnlMiddle;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Panel pnlBtm;
+        private System.Windows.Forms.Timer TimerFirstFloorDoorOpen;
+        private System.Windows.Forms.Timer TimerFirstFloorDoorClose;
+        private System.Windows.Forms.Timer TimerGFloorDoorOpen;
+        private System.Windows.Forms.Timer TimerGFloorDoorClose;
+        private System.Windows.Forms.Timer TimerLiftDown;
+        private System.Windows.Forms.Timer TimerLiftUp;
+    }
+}
+
---
+++ WoodenElevatorProject/WoodenElevatorProject/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WoodenElevatorProject
+{
+    public partial class Form1 : Form
+    {
+        int yLiftUp = -2;
+        int yLiftDown = 263;
+        int xLeftDoorClose = -24;
+        int xLeftDoorOpen = -105;
+        int xRightDoorClose = 83;
+        int xRightDoorOpen = 159;
+
+        bool liftGoUp = false;
+        bool liftGoDown = false;
+
+        bool arrivedAtGf = false;
+        bool arrivedAt1f = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void TimerFirstFloorDoorOpen_Tick(object sender, EventArgs e)
+        {
+            if (picUprLeftDoor.Left >= xLeftDoorOpen && picUprRightDoor.Left <= xRightDoorOpen)
+            {
+                picUprLeftDoor.Left -= 1;
+                picUprRightDoor.Left += 1;
+            }
+            else
+            {
+                TimerFirstFloorDoorOpen.Enabled = false;
+            }
+        }
+
+        private void TimerFirstFloorDoorClose_Tick(object sender, EventArgs e)
+        {
+            if (picUprLeftDoor.Left <= xLeftDoorClose && picUprRightDoor.Left >= xRightDoorClose)
+            {
+                picUprLeftDoor.Left += 1;
+                picUprRightDoor.Left -= 1;
+            }
+            else
+            {
+                TimerFirstFloorDoorClose.Enabled = false;
+
+                if (liftGoDown == true)
+                {
+                   TimerLiftDown.Enabled = true;
+                    liftGoDown = false;
+                }
+            }
+        }
+
+        private void TimerLiftDown_Tick(object sender, EventArgs e)
+        {
+            if (pnlLift.Top <= yLiftDown)
+            {
+                pnlLift.Top += 1;
+            }
+            else
+            {
+                TimerLiftDown.Enabled = false;
+                btnUp.Enabled = true;
+                btn1.Enabled = true;
+                btnOpen.Enabled = true;
+                btnClose.Enabled = true;
+                btnDown.BackColor = Color.White;
+                btnG.BackColor = Color.White;
+                GFloorDoorOpen();
+                arrivedAtGf = true;
+
+                //pnlLift.Image = global::ElevatorControl.Properties.Resources.Inside_of_the_lift;
+
+                //display_panel.Image = global::ElevatorControl.Properties.Resources._1;
+                //display_top.Image = global::ElevatorControl.Properties.Resources._1;
+                //display_bottom.Image = global::ElevatorControl.Properties.Resources._1;
+            }
+
+
+        }
+
+        private void TimerLiftUp_Tick(object sender, EventArgs e)
+        {
+            if (pnlLift.Top >= yLiftUp)
+            {
+                pnlLift.Top -= 1;
+            }
+            else
+            {
+                TimerLiftUp.Enabled = false;
+                btnDown.Enabled = true;
+                btnG.Enabled = true;
+                btnClose.Enabled = true;
+                btnOpen.Enabled = true;
+                btnUp.BackColor = Color.White;
+                btn1.BackColor = Color.White;
+
+                FirstFloorDoorOpen();
+                arrivedAt1f = true;
+            }
+        }
+
+        private void TimerGFloorDoorOpen_Tick(object sender, EventArgs e)
+        {
+            if (picLwrLeftDoor.Left >= xLeftDoorOpen && picLwrRightDoor.Left <= xRightDoorOpen)
+            {
+                picLwrLeftDoor.Left -= 1;
+                picLwrRightDoor.Left += 1;
+            }
+            else
+            {
+                TimerGFloorDoorOpen.Enabled = false;
+            }
+        }
+
+        private void TimerGFloorDoorClose_Tick(object sender, EventArgs e)
+        {
+            if (picLwrLeftDoor.Left <= xLeftDoorClose && picLwrRightDoor.Left >= xRightDoorClose)
+            {
+                picLwrLeftDoor.Left += 1;
+                picLwrRightDoor.Left -= 1;
+            }
+            else
+            {
+                TimerGFloorDoorClose.Enabled = true;
+
+                if (liftGoUp == true)
+                {
+                    TimerLiftUp.Enabled = true;
+                    liftGoUp = false;
+                }
+            }
+        }
+
+        private void GFloorDoorClose()
+        {
+            TimerGFloorDoorClose.Enabled = true;
+            TimerGFloorDoorOpen.Enabled = false;
+        }
+        private void GFloorDoorOpen()
+        {
+            TimerGFloorDoorClose.Enabled = false;
+            TimerGFloorDoorOpen.Enabled = true;
+        }
+
+        private void FirstFloorDoorClose()
+        {
+            TimerFirstFloorDoorClose.Enabled = true;
+            TimerFirstFloorDoorOpen.Enabled = false;
+
+        }
+
+        private void FirstFloorDoorOpen()
+        {
+            TimerFirstFloorDoorClose.Enabled = false;
+            TimerFirstFloorDoorOpen.Enabled = true;
+        }
+
+        private void LiftGoingUp()
+        {
+            liftGoUp = true;
+            GFloorDoorClose();
+            btnG.Enabled = false;
+            btnDown.Enabled = false;
+            btnClose.Enabled = false;
+            btnOpen.Enabled = false;
+            arrivedAtGf = false;
+        }
+
+        private void LiftGoingDown()
+        {
+            liftGoDown = true;
+            FirstFloorDoorClose();
+            btn1.Enabled = false;
+            btnUp.Enabled = false;
+            btnClose.Enabled = false;
+            btnOpen.Enabled = false;
+            arrivedAt1f = false;
+
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            if (arrivedAt1f == true)
+            {
+                FirstFloorDoorOpen();
+            }
+            else if (arrivedAtGf == true)
+            {
+                GFloorDoorOpen();
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            if (arrivedAtGf == true)
+            {
+                GFloorDoorClose();
+            }
+            else if (arrivedAt1f == true)
+            {
+                FirstFloorDoorClose();
+            }
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            btn1.BackColor = Color.Red;
+            LiftGoingUp();
+        }
+
+        private void btnG_Click(object sender, EventArgs e)
+        {
+            btnG.BackColor = Color.Red;
+            LiftGoingDown();
+        }
+
+        private void btnAlarm_Click(object sender, EventArgs e)
+        {
+            btnAlarm.BackColor = Color.Green;
+            TimerFirstFloorDoorOpen.Enabled = true;
+            TimerGFloorDoorOpen.Enabled = true;
+            TimerFirstFloorDoorClose.Enabled = false;
+            TimerGFloorDoorClose.Enabled = false;
+        }
+
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            LiftGoingUp();
+            
+        }
+
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+            LiftGoingDown();
+        }
+    }
+}
